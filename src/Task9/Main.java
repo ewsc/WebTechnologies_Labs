3cefb93f32175417e041f8d480a716ef4e5c85fe
gthanksg <alamovazamjon@gmail.com>
2023-10-13 03:27:51 +0300

    Tarea diez y once

--- src/Task9/Main.java
+++ src/Task9/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Ball ball1 = new Ball(1, Color.GREEN);
         Ball ball2 = new Ball(2, Color.BLUE);
-        Ball ball3 = new Ball(3, Color.GREEN);
-        Ball ball4 = new Ball(13, Color.GREEN);
+        Ball ball3 = new Ball(7, Color.BLUE);
+        Ball ball4 = new Ball(4, Color.BLUE);
+        Ball ball5 = new Ball(3, Color.GREEN);
+        Ball ball6 = new Ball(13, Color.GREEN);
 
         Bucket bucket = new Bucket(22);
 
@@ -15,6 +17,8 @@ public class Main {
         bucket.addBall(ball2);
         bucket.addBall(ball3);
         bucket.addBall(ball4);
+        bucket.addBall(ball5);
+        bucket.addBall(ball6);
 
         System.out.println(bucket.countBlue());
     }
