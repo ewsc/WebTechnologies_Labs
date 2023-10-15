3f2a6a7fd32335607e03ea59dddfcfaed29a7aa7
gthanksg <alamovazamjon@gmail.com>
2023-10-15 03:41:15 +0300

    Tarea dieciséis

---
+++ src/Task16/comparator/TitleComparator.java
@@ -0,0 +1,12 @@
+package Task16.comparator;
+
+import Task16.Book;
+
+import java.util.Comparator;
+
+public class TitleComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.getTitle().compareTo(o2.getTitle());
+    }
+}
