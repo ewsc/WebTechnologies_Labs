7f751d6524e92f78a193db8db40c2b2895f7bec3
gthanksg <alamovazamjon@gmail.com>
2023-10-22 02:42:52 +0300

    Se agregaron pruebas unitarias para la tarea catorce

--- src/xyz/alamov/Task14/Book.java
+++ src/xyz/alamov/Task14/Book.java
@@ -1,10 +1,25 @@
 package xyz.alamov.Task14;
 
-public class Book {
+public class Book implements Cloneable {
     private String title;
     private String author;
     private int price;
     private static int edition;
+
+    public Book(String title, String author, int price, int edition){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        setEdition(edition);
+    }
+
+    public void setEdition(int edition) {
+        Book.edition = edition;
+    }
+
+    public static int getEdition() {
+        return edition;
+    }
 
     @Override
     public int hashCode() {
@@ -25,12 +40,13 @@ public class Book {
 
     @Override
     public Object clone() {
-        Book clonedBook = new Book();
-        clonedBook.author = author;
-        clonedBook.title = title;
-        clonedBook.price = price;
-        clonedBook.edition = edition;
-        return clonedBook;
+        try {
+            Book clonedBook = (Book) super.clone();
+            clonedBook.setEdition(getEdition());
+            return clonedBook;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("CloneNotSupportedException should not occur.");
+        }
     }
 
     @Override
