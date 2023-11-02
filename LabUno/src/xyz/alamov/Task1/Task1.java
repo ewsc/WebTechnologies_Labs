71d1342b87bf76a5eaf6d84ec272ab66759046c6
Azam Alamov <alamovazamjon@gmail.com>
2023-11-02 16:38:42 +0300

    Delete LabUno directory

--- LabUno/src/xyz/alamov/Task1/Task1.java
+++
@@ -1,9 +0,0 @@
-package xyz.alamov.Task1;
-
-public class Task1 {
-    public static double calc(double x, double y) {
-        double a = (1 + Math.sin(x + y) * Math.sin(x + y));
-        double b = 2 + Math.abs(x - (2 * x) / (1 + x * x * y * y));
-        return a / b + x;
-    }
-}
