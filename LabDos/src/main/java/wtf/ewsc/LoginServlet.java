61781a4086c0a5b0067173e630dbb94429a9c412
ewsc <alamovazamjon@gmail.com>
2023-11-24 21:38:28 +0300

    login error message added

--- LabDos/src/main/java/wtf/ewsc/LoginServlet.java
+++ LabDos/src/main/java/wtf/ewsc/LoginServlet.java
@@ -8,11 +8,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String error = null;
+        if (Objects.equals(request.getParameter("error"), "1")) {
+            error = "Wrong username or password.";
+        }
+        request.setAttribute("error", error);
         RequestDispatcher dispatcher = request.getRequestDispatcher("pages/login.jsp");
         dispatcher.forward(request, response);
     }
