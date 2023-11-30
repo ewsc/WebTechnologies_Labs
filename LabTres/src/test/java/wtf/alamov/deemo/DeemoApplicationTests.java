ba9c69893cb68baf8ae07ddbbfaff4064f315ace
Azam Alamov <alamovazamjon@gmail.com>
2023-11-30 18:15:50 +0300

    added LabTres

---
+++ LabTres/src/test/java/wtf/alamov/deemo/DeemoApplicationTests.java
@@ -0,0 +1,13 @@
+package wtf.alamov.deemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
